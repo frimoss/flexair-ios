adc86c3c9f577151ab09937b7c29488059e19097
frimoss <78547341+frimoss@users.noreply.github.com>
2025-10-21 20:08:58 +0300

    Create Xcode project

---
+++ flexair/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  flexair
+//
+//  Created by user on 21/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ flexair/flexairApp.swift
@@ -0,0 +1,17 @@
+//
+//  flexairApp.swift
+//  flexair
+//
+//  Created by user on 21/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct flexairApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
