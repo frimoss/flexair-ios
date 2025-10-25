4d4457075930d14588336b07dca51a02aa474c82
frimoss <frimoss@yandex.ru>
2025-10-25 19:49:10 +0300

    Create SearchFlightsView

---
+++ flexair/Scenes/Flights/SearchFlights/SearchFlightsView.swift
@@ -0,0 +1,56 @@
+//
+//  SearchFlightsView.swift
+//  flexair
+//
+//  Created by Nikolai on 25/10/2025.
+//
+
+import SwiftUI
+
+struct SearchFlightsView: View {
+    
+    @State private var fromWhere: String = "Istanbul, IST"
+    @State private var toWhere: String = ""
+    
+    var body: some View {
+        NavigationStack {
+            VStack(alignment: .leading, spacing: 24) {
+                VStack(spacing: 12) {
+                    // From
+                    SearchFlightButtonView(title: "From", userInput: fromWhere) {
+                        
+                    }
+                    // To where
+                    SearchFlightButtonView(title: "To", userInput: toWhere) {
+                        
+                    }
+                    
+                    HStack(spacing: 12) {
+                        // Travel Dates
+                        SearchFlightButtonView(title: "Travel dates", image: "Calendar") {
+                            
+                        }
+                        // Passenger
+                        SearchFlightButtonView(title: "1 pass, Economy", image: "Passenger", isPrimaryColor: true) {
+                            
+                        }
+                    }
+                    
+                }
+
+                Button("Find tickets") {
+                    
+                }
+                .buttonStyle(PrimaryButtonStyle())
+                
+                Spacer()
+            }
+            .navigationTitle("Flexair")
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    SearchFlightsView()
+}
