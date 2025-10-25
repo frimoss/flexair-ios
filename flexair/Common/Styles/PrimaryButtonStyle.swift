ae9fb8f1bacf5fc039785658f65eed32c72a4c17
frimoss <frimoss@yandex.ru>
2025-10-25 19:36:29 +0300

    Create: PrimaryButton Style

---
+++ flexair/Common/Styles/PrimaryButtonStyle.swift
@@ -0,0 +1,38 @@
+//
+//  PrimaryButtonStyle.swift
+//  flexair
+//
+//  Created by Nikolai on 25/10/2025.
+//
+
+import SwiftUI
+
+struct PrimaryButtonStyle: ButtonStyle {
+    
+    var isEnabled: Bool = true
+    
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            // Font Size
+            .font(.custom(Constants.Fonts.regular, size: 17)) // TODO: Change Font Size
+        
+            // Styles
+            .frame(maxWidth: .infinity)
+            .frame(height: Constants.UI.height)
+        
+            .foregroundStyle(Constants.Colors.textAccent)
+            .background(Constants.Colors.accent)
+            
+            .clipShape(RoundedRectangle(cornerRadius: Constants.UI.cornerRadius))
+        
+            // Hover Effect
+            .opacity(configuration.isPressed ? 0.8 : 1.0)
+            .scaleEffect(configuration.isPressed ? 0.98 : 1.0)
+            .animation(.easeInOut(duration: 0.1), value: configuration.isPressed)
+        
+            // Disabled
+            .opacity(
+                isEnabled ? 1 : 0.8
+            )
+    }
+}
