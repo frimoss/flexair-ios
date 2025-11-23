ee9b85e8837d3e08be88dbb1177ed704b9f3c881
frimoss <frimoss@yandex.ru>
2025-11-23 21:47:02 +0200

    Update FlightSearchView

--- flexair/Features/Flights/FlightSearch/View/FlightSearchView.swift
+++ flexair/Features/Flights/FlightSearch/View/FlightSearchView.swift
@@ -8,19 +8,26 @@
 import SwiftUI
 
 struct FlightSearchView: View {
-
-    @State private var departure: String = "Istanbul, IST"
-    @State private var destination: String = ""
+    
+    @State private var flightVM = FlightSearchViewModel()
+    @State private var navigateToFlights = false
+    
+    // Airports
+    @State private var originAirport: Airport? = airportData[0] // Default: "Istanbul, IST"
+    @State private var destinationAirport: Airport? = nil
+    
+    @State private var showAirportListOrigin = false
+    @State private var showAirportListDestination = false
+    
+    // Date
     @State private var selectedDate = Date()
     @State private var selectedDateText = "Travel dates"
-    
-    // Flags
-    @State private var showAirportListDeparture = false
-    @State private var showAirportListDestination = false
-    @State private var showDatePicker = false
     @State private var isDateSelected = false
+    @State private var showDatePicker = false
     
-    @State private var showPassengers = false
+    // Alert
+    @State private var showAlert = false
+    @State private var alertMessage = ""
     
     var body: some View {
         NavigationStack {
@@ -30,19 +37,19 @@ struct FlightSearchView: View {
                         ZStack(alignment: .trailing) {
                             VStack(spacing: 12) {
                                 // From
-                                SearchFlightButtonView(title: "From", userInput: departure) {
-                                    showAirportListDeparture = true
+                                SearchFlightButtonView(title: "From", userInput: originAirport?.displayName ?? "") {
+                                    showAirportListOrigin = true
                                 }
                                 // To where
-                                SearchFlightButtonView(title: "To", userInput: destination) {
+                                SearchFlightButtonView(title: "To", userInput: destinationAirport?.displayName ?? "") {
                                     showAirportListDestination = true
                                 }
                             }
                             
                             Button {
-                                let text = departure
-                                departure = destination
-                                destination = text
+                                let airport = originAirport
+                                originAirport = destinationAirport
+                                destinationAirport = airport
                             } label: {
                                 Image(systemName: "arrow.up.arrow.down")
                             }
@@ -63,10 +70,55 @@ struct FlightSearchView: View {
                         
                     }
                     
-                    NavigationLink {
-                        FlightListView()
-                    } label: {
-                        Text("Find tickets")
+                    Button("Get tickets") {
+                        guard let originCode = originAirport?.code else {
+                            alertMessage = "Please select origin Airport."
+                            showAlert = true
+                            return
+                        }
+                        
+                        guard let destinationCode = destinationAirport?.code else {
+                            alertMessage = "Please select destination Airport."
+                            showAlert = true
+                            return
+                        }
+                        
+                        if !isDateSelected {
+                            alertMessage = "Please select the flight date."
+                            showAlert = true
+                            return
+                        }
+                        
+                        let departureDate = selectedDate.toAPIFormat()
+                        
+                        print()
+                        print("Origin: \(originCode)")
+                        print("Destination: \(destinationCode)")
+                        print("Date: \(departureDate)")
+                        
+                        flightVM.origin = originCode
+                        flightVM.destination = destinationCode
+                        flightVM.departureDate = departureDate
+                        
+                        Task {
+                            await flightVM.searchFlights()
+                            
+                            print("✅ Found \(flightVM.flights.count) flights")
+                            
+                            if flightVM.flights.count == 0 {
+                                alertMessage = "Flights were not found. Try to change Airport."
+                                showAlert = true
+                            } else {
+                                // MARK: Navigation to Flight List
+                                navigateToFlights = true
+                            }
+                            
+                            if let error = flightVM.errorMessage {
+                                alertMessage = error
+                                showAlert = true
+                            }
+                        }
+                        
                     }
                     .buttonStyle(PrimaryButtonStyle())
                     
@@ -76,17 +128,30 @@ struct FlightSearchView: View {
             }
             .navigationTitle("Flexair")
             .navigationBarTitleDisplayMode(.large)
-            .sheet(isPresented: $showAirportListDeparture) {
-                AirportListView(navTitle: "From", userInput: $departure)
+            .navigationBarBackButtonHidden()
+            .background(Constants.Colors.backgroundApp)
+            .sheet(isPresented: $showAirportListOrigin) {
+                AirportListView(selected: $originAirport, title: "From")
             }
             .sheet(isPresented: $showAirportListDestination) {
-                AirportListView(navTitle: "To", userInput: $destination)
+                AirportListView(selected: $destinationAirport, title: "To")
             }
             .sheet(isPresented: $showDatePicker) {
                 DatePickerView(selectedDate: $selectedDate, selectedDateText: $selectedDateText, isDateSelected: $isDateSelected)
             }
-            .navigationBarBackButtonHidden()
-            .background(Constants.Colors.backgroundApp)
+            .navigationDestination(isPresented: $navigateToFlights) {
+                if let originCity = originAirport?.city, let destinationCity = destinationAirport?.city {
+                    
+                    let title = "\(originCity) — \(destinationCity)" // "Istanbul — Los Angeles"
+                    
+                    FlightListView(flights: flightVM.flights, title: title)
+                }
+            }
+            .alert("Error", isPresented: $showAlert) {
+                Button("OK", role: .cancel) { }
+            } message: {
+                Text(alertMessage)
+            }
         }
     }
 }
