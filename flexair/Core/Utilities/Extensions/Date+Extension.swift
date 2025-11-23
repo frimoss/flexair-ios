47085f1b4d8e3acfaec0aa0413997a022bb9b986
frimoss <frimoss@yandex.ru>
2025-11-23 21:47:54 +0200

    Update Date+Extension

--- flexair/Core/Utilities/Extensions/Date+Extension.swift
+++ flexair/Core/Utilities/Extensions/Date+Extension.swift
@@ -2,7 +2,18 @@
 //  Date+Extension.swift
 //  flexair
 //
-//  Created by user on 23/11/2025.
+//  Created by Nikolai on 23/11/2025.
 //
 
 import Foundation
+
+extension Date {
+    /// Convert Date to "2025-11-23" format for API
+    func toAPIFormat() -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        formatter.timeZone = TimeZone(secondsFromGMT: 0)
+        return formatter.string(from: self)
+    }
+}
