85b401c50d0dd938ea0badb0f5d52849e5e61332
frimoss <frimoss@yandex.ru>
2025-12-16 18:03:54 +0200

    Create FlightListView

---
+++ flexair/Module/Flights/FlightList/FlightListView.swift
@@ -0,0 +1,64 @@
+//
+//  FlightListView.swift
+//  flexair
+//
+//  Created by Nikolai on 22/10/2025.
+//
+
+import SwiftUI
+
+struct FlightListView: View {
+    
+    // MARK: - Public
+    let flights: [Flight]
+    var title: String
+    @Binding var path: NavigationPath
+    
+    // MARK: - Private
+    @State private var navigateToBooking = false
+    @Environment(\.dismiss) private var dismiss
+    
+    // MARK: - TODO: Put in Constants
+    private let titleSize: CGFloat = 15
+    private let subtitleSize: CGFloat = 13
+    
+    var body: some View {
+        ScrollView {
+            LazyVStack {
+                ForEach(0..<6) { index in
+                    ForEach(flights) { flight in
+                        Button {
+                            path.append(FlightPage.booking(flight: flight))
+                            Haptics.light()
+                        } label: {
+                            FlightListItemView(flight: flight)
+                        }
+                        .padding()
+                        .foregroundStyle(Constants.Colors.textPrimary)
+                        .background(
+                            RoundedRectangle(cornerRadius: Constants.UI.cornerRadius)
+                                .fill(Constants.Colors.background)
+                        )
+                    }
+                }
+            }
+            .padding()
+        }
+        .scrollIndicators(.hidden)
+        .background(Constants.Colors.backgroundApp)
+        .navigationTitle(title)
+        .navigationBarTitleDisplayMode(.inline)
+        .navigationBarBackButtonHidden()
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                Button {
+                    dismiss()
+                } label: {
+                    Image(systemName: "chevron.backward")
+                        .fontWeight(.semibold)
+                }
+            }
+        }
+    }
+}
+
---
+++ flexair/Module/Flights/FlightList/Part/FlightListItemView.swift
@@ -0,0 +1,83 @@
+//
+//  FlightListItemView.swift
+//  flexair
+//
+//  Created by Nikolai on 23/11/2025.
+//
+
+import SwiftUI
+
+struct FlightListItemView: View {
+    
+    var flight: Flight
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 16) {
+            HStack(alignment: .top) {
+                
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("$\(flight.price)")
+                        .font(.system(size: 25, weight: .heavy))
+                    
+                    Text("$\(flight.flightPrice + flight.baggagePrice) including baggage 1×23 kg")
+                        .font(.system(size: 14, weight: .regular))
+                        .padding(3)
+                        .padding(.horizontal, 2)
+                        .background(
+                            .gray
+                                .opacity(0.2)
+                        )
+                        .clipShape(RoundedRectangle(cornerRadius: Constants.UI.cornerRadius))
+                }
+                
+                Spacer()
+                
+                Button {
+                    //
+                } label: {
+                    Image(systemName: "heart")
+                        .font(.system(size: 15, weight: .medium))
+                        .foregroundColor(Constants.Colors.textPrimary)
+                        .padding(10)
+                        .background(
+                            .gray
+                            .opacity(0.2)
+                        )
+                        .clipShape(Circle())
+                        .shadow(radius: 8)
+                }
+            }
+            
+            HStack(alignment: .center, spacing: 12) {
+                Image("TurkishAirlines")
+                    .resizable()
+                    .frame(width: 25, height: 25)
+                
+                HStack(alignment: .top, spacing: 1) {
+                    VStack(alignment: .leading) {
+                        Text(flight.formattedDepartureTime)
+                        
+                        Text(flight.departureAirportCode) // MARK: Departure Code
+                            .foregroundStyle(.gray)
+                    }
+                    Text("—")
+                        .foregroundStyle(.gray)
+                    
+                    VStack(alignment: .leading) {
+                        Text(flight.formattedArrivalTime)
+                        
+                        Text(flight.arrivalAirportCode) // MARK: Destination Code
+                            .foregroundStyle(.gray)
+                    }
+                    
+                    HStack(alignment: .top, spacing: 4) {
+                        Text("Travel time: \(flight.duration)")
+                    }
+                    .padding(.leading, 14)
+                }
+                
+            }
+            .font(.system(size: 14, weight: .regular))
+        }
+    }
+}
