321549d9f0a6982c4a950799fa531417514f569e
frimoss <frimoss@yandex.ru>
2025-12-16 18:32:33 +0200

    Update Shared UI Components

---
+++ flexair/Resources/Shared/Components/SearchFlightButtonView.swift
@@ -0,0 +1,48 @@
+//
+//  SearchFlightButtonView.swift
+//  flexair
+//
+//  Created by Nikolai on 25/10/2025.
+//
+
+import SwiftUI
+
+struct SearchFlightButtonView: View {
+    var title: String
+    var userInput: String = ""
+    var image: String = ""
+    var isPrimaryColor: Bool = false
+    
+    var action: () -> Void
+    
+    var body: some View {
+        Button {
+            action()
+        } label: {
+            HStack(alignment: .center, spacing: 8) {
+                if !image.isEmpty {
+                    Image(image)
+                }
+                
+                VStack(alignment: .leading, spacing: 2) {
+                    if !userInput.isEmpty {
+                        Text(title)
+                            .foregroundStyle(Constants.Colors.textSecondary)
+                            .font(.system(size: 13, weight: .regular))
+                        
+                        Text(userInput)
+                            .foregroundStyle(Constants.Colors.textPrimary)
+                            .font(.system(size: 15, weight: .medium))
+                    } else {
+                        Text(title)
+                            .foregroundStyle(isPrimaryColor ? Constants.Colors.textPrimary : Constants.Colors.textSecondary)
+                            .font(.system(size: 14, weight: .medium))
+                    }
+                }
+                Spacer()
+            }
+            .frame(maxWidth: .infinity)
+        }
+        .buttonStyle(SecondaryButtonStyle())
+    }
+}
---
+++ flexair/Resources/Shared/Styles/PrimaryTextFieldStyle.swift
@@ -0,0 +1,23 @@
+//
+//  PrimaryTextFieldStyle.swift
+//  flexair
+//
+//  Created by Nikolai on 24/10/2025.
+//
+
+import SwiftUI
+
+struct PrimaryTextFieldStyle: TextFieldStyle {
+    
+    func _body(configuration: TextField<_Label>) -> some View {
+        configuration
+            .padding(.horizontal) // Internal padding
+            .frame(height: Constants.UI.height)
+            .foregroundStyle(Constants.Colors.textPrimary)
+            .background(Constants.Colors.background)
+            .clipShape(RoundedRectangle(cornerRadius: Constants.UI.cornerRadius))
+            .onTapGesture {
+                Haptics.light()
+            }
+    }
+}
