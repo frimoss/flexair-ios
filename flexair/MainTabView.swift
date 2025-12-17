9c19446e11a5909d793721d52c89554e3968eecd
frimoss <frimoss@yandex.ru>
2025-12-17 10:36:10 +0200

    Update MainTabView

--- flexair/MainTabView.swift
+++ flexair/MainTabView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  MainTabView.swift
 //  flexair
 //
 //  Created by Nikolai on 21/10/2025.
@@ -8,36 +8,62 @@
 import SwiftUI
 
 enum AppTab: Hashable {
-    case home, tickets, profile
+    case home, userBookings, favorites, profile
+}
+
+enum FlightPage: Hashable {
+    case list(flights: [Flight], title: String)
+    case booking(flight: Flight)
 }
 
 struct MainTabView: View {
-    
     @State private var selection: AppTab = .home
+    
+    @State private var homeNavigationPath = NavigationPath()
+    @State private var bookingNavigationPath = NavigationPath()
     
     var body: some View {
         TabView(selection: $selection) {
             Tab("Flights", systemImage: "airplane", value: AppTab.home) {
-                NavigationStack {
-                    SearchFlightsView()
+                NavigationStack(path: $homeNavigationPath) {
+                    FlightSearchView(path: $homeNavigationPath)
+                    .navigationDestination(for: FlightPage.self) { page in
+                        switch page {
+                        case .list(let flights, let title):
+                            FlightListView(flights: flights, title: title, path: $homeNavigationPath)
+                        case .booking(let flight):
+                            FlightBookingView(
+                                flight: flight,
+                                homeNavigationPath: $homeNavigationPath,
+                                bookingNavigationPath: $bookingNavigationPath,
+                                tab: $selection
+                            )
+                        }
+                    }
                 }
                 .tag(AppTab.home)
             }
             
-            Tab("Tickets", systemImage: "barcode.viewfinder", value: AppTab.tickets) {
-                NavigationStack {
-                    //
+            Tab("Bookings", systemImage: "barcode.viewfinder", value: AppTab.userBookings) {
+                NavigationStack(path: $bookingNavigationPath) {
+                    UserBookingsView()
+                        .navigationDestination(for: UserBooking.self) { booking in
+                            UserBookingItemView(userBooking: booking)
+                        }
                 }
-                .tag(AppTab.tickets)
+                .tag(AppTab.userBookings)
             }
             
             Tab("Profile", systemImage: "person.crop.circle", value: AppTab.profile) {
                 NavigationStack {
-                    //
+                    //HomeView()
+                    ProfileView()
                 }
                 .tag(AppTab.profile)
             }
         }
+        .toolbarBackground(.visible, for: .tabBar)
+        .toolbarBackground(Color(.systemBackground), for: .tabBar)
     }
 }
 
